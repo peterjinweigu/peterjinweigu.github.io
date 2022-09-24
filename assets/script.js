e3e5d5067b818576374fc307a4ccb3d04493bb47
peterjinweigu <petergujinwei@gmail.com>
2022-09-24 13:06:18 -0700

    start-2

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+/* https://rhhsweb.github.io/ (Thanks Edison :D) */
+
+const typingElement = document.getElementById("typing-animation");
+const sentence = "Hey! My name is Peter Gu";
+let letter = 0;
+const animation = setInterval(()=> {
+    typingElement.textContent += sentence[letter++];
+    if (letter == sentence.length) {
+        clearInterval(animation);
+    }
+}, 100);
+
+
+
+
